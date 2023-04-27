5a6162169849306427af2089fe6710675e8c9a25
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-04-27 12:23:10 +0530

    the main component for the parallax effect

---
+++ src/TransformHeroes.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+
+function TransformHeroes() {
+  useEffect(() => {
+    const elements = document.querySelectorAll('.card');
+    const perspective = '500px';
+    const delta = 20;
+
+    elements.forEach((element) => {
+      const width = element.offsetWidth;
+      const height = element.offsetHeight;
+      const midWidth = width / 2;
+      const midHeight = height / 2;
+
+      const handleMouseMove = (e) => {
+        const pos = element.getBoundingClientRect();
+        const cursPosX = (e.pageX - pos.left) + 20;
+        const cursPosY = e.pageY - pos.top + 20;
+        const cursCenterX = midWidth - cursPosX;
+        const cursCenterY = midHeight - cursPosY;
+
+        element.style.transform = `perspective(${perspective}) rotateX(${cursCenterY / delta}deg) rotateY(${-cursCenterX / delta}deg)`;
+        element.classList.remove('is-out');
+      };
+
+      const handleMouseLeave = () => {
+        element.classList.add('is-out');
+      };
+
+      element.addEventListener('mousemove', handleMouseMove);
+      element.addEventListener('mouseleave', handleMouseLeave);
+
+      return () => {
+        element.removeEventListener('mousemove', handleMouseMove);
+        element.removeEventListener('mouseleave', handleMouseLeave);
+      };
+    });
+  }, []);
+
+  return null;
+}
+
+export default TransformHeroes;
